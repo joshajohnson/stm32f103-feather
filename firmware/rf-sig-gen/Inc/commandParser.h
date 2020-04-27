42e31639a8cd5122bab8844adde0e8a2588a6502
Josh Johnson <josh@joshajohnson.com>
2020-04-27 10:34:55 +1000

    move siggen firmware

--- firmware/rf-sig-gen/Inc/commandParser.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMANDPARSER_H_
-#define COMMANDPARSER_H_
-
-extern struct MAX2871Struct max2871Status;
-extern struct txStruct txStatus;
-
-void commandParser(struct MAX2871Struct *max2871Status, struct txStruct *txStatus);
-
-#endif
--- firmware/rf-sig-gen/build/max2871_registers.d
+++
@@ -1,4 +0,0 @@
-build/max2871_registers.o: Src/max2871_registers.c \
- Inc/max2871_registers.h
-
-Inc/max2871_registers.h:
